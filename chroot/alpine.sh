7f955009511c3b889cd0b1481afd334672ea84e5
smplusr <83000221+smplusr@users.noreply.github.com>
2022-06-05 23:33:58 +0200

    update and upload
    major updates to alpine.sh
    adding exchange.sh

--- chroot/alpine.sh
+++ chroot/alpine.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 web="http://dl-cdn.alpinelinux.org/alpine/v3.16/releases/x86_64/alpine-virt-3.16.0-x86_64.iso"
-rom="alpine-virt-3.16.0-x86_64.iso"
+iso="alpine-virt-3.16.0-x86_64.iso"
 
-len="128M"
 mem="512M"
 
+help="\n
+	Virtualized Alpine system. Runs a fully functionnal Live CDROM only Alpine linux guest. \n
+	Requires qemu-system-x86_64. \n
+	Option (all optionnal) : \n
+		-m	memory (RAM), default : '$mem' \n
+		-f	live ISO, default : '$iso' \n
+		-k	enables KVM acceleration, default : 'UNSET' \n
+		-h	help \n	"
 
-while getopts ":m:l:f:r:k" opt; do
+
+
+while getopts ":m:f:k:h" opt; do
 	case "$opt" in
 		m)
 			mem=$OPTARG
 			;;
-        l)
-			len=$OPTARG
-			;;
 		f)
-			hda=$OPTARG
-			;;
-		r)
-			rom=$OPTARG
+			iso=$OPTARG
 			;;
 		k)
 			kvm="true"
+			;;
+		h)
+			echo -e $help
+			exit 0
 			;;
 	esac
 done
 
 
-if ! test -f $rom; then
+if ! test -f $iso; then
 	wget $web
 fi
 
@@ -35,20 +42,14 @@ command="$command \
 	qemu-system-x86_64 \
 		-nographic \
 		-m $mem -smp $(nproc) \
-        -machine type=q35,accel=tcg,usb=off,dump-guest-core=off \
-		-drive file=$rom,media=cdrom,cache=writeback \
+		-net nic -net user,hostfwd=tcp::2222-:22 \
+        	-machine type=q35,accel=tcg,usb=off,dump-guest-core=off \
+		-drive file=$iso,media=cdiso,cache=writeback \
 		-boot d
 	"
 
 if ! test -z $kvm; then
-	command="$command -machine type=q35,accel=kvm -cpu host"
-fi
-
-if ! test -z $hda; then
-	if ! test -f $hda; then
-		eval "qemu-img create -f raw $hda $len"
-	fi
-	command="$command -drive file=$hda,if=virtio,format=raw,cache=writeback"
+	command="$command -enable-kvm"
 fi
 
 
---
+++ chroot/exchange.sh
@@ -0,0 +1,64 @@
+user="root"
+addr="localhost"
+port="22"
+dir="./"
+
+help="\n
+	File exchanger using OpenSSH's SCP file transfer protocol. \n
+	Requires networking and OpenSSH. \n
+	Warning : \n
+		-	on qemu machines, requires TCP port forwarding \n
+		-	requires setting a password and authorisations for root users \n
+		-	deleting ~/.ssh/known_hosts might be obligatory \n
+	Options : \n
+		-u	user, default : '$user' \n
+		-a	address (IP), default : '$addr' (127.0.0.1) \n
+		-p	port, default : '$port' \n
+		-f	directory, default : '$dir' \n
+		-o	operation, default : 'UNSET' \n	"
+
+
+
+while getopts ":u:a:p:f:o:h" opt; do
+	case "$opt" in
+		u)
+			user=$OPTARG
+			;;
+		a)
+			addr=$OPTARG
+			;;
+		p)
+			port=$OPTARG
+			;;
+		f)
+			dir=$OPTARG
+			;;
+		o)
+			operation=$OPTARG
+			;;
+		h)
+			echo -e $help
+			exit 0
+			;;
+	esac
+done
+
+
+if test -z $operation; then
+	read -p "Please specify an operation : " operation
+fi
+
+
+if test $operation == "set"; then
+	eval "scp -r -P $port ${dir}/* ${addr}:/root/"
+	exit 0
+fi
+
+if test $operation == "get"; then
+	eval "scp -r -P $port ${addr}:/root/* $dir"
+	exit 0
+fi
+
+
+echo "Invalid Operation"
+exit 1
